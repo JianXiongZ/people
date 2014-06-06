e0f06b28e661f244e333b87b973408e4be9aecfb
archangdcc <archang@0xf8.com>
2014-06-06 17:18:11 +0800

    new data structure

--- statreport/chkstat.py
+++ statreport/chkstat.py
@@ -5,85 +5,136 @@ import time
 
 def chkstat(hosts):
 
-    dev_flag = re.compile('MHS[^,=]*=0[.]00,')
-    pool_flag = re.compile('Status=Alive')
-
-    err_hosts=[]
-    err_devs=[]
-    err_pools=[]
-
-    stats={}
-    #dev_stats=[]
-    #pool_stats=[]
+	elapsed_time_flag	= re.compile('Device Elapsed=(.*)$')
+	total_Mh_flag		= re.compile('Total MH=([^,]*),')
+	temperature_d_flag	= re.compile('Temperature=([^,]*),')
+	module_id_flag		= re.compile('ID\d MM Version')
+	temperature_s_flag	= re.compile('Temperature(\d=[^,]*),')
+	fan_flag			= re.compile('Fan(\d=[^,]*),')
+	pool_url_flag		= re.compile('URL=([^,]*),')
+	pool_alive_flag		= re.compile('Status=([^,]*),')
+	
+	#ignore pool stat in ' -o stats'
+	stat_pool_flag		= re.compile('ID=POOL')
+	
+	data = []
     
-    i = 1
-    for h in hosts:
+	i = 0
+	for h in hosts:
 	
-	tn = telnetlib.Telnet()
+		tn = telnetlib.Telnet()
 
-	conn_flag = False
-	for k in range(0,5):
-	    try:
-		print 'Conneting ' + h +' ...',
-		tn.open(h,23)
-		print 'Done.'
-		break
-	    except:
-		tn.close()
-		print 'Error' + ('. Try Again.' if k < 4 else '. Skip.')
-		conn_flag = True
-	if conn_flag:
-	    err_hosts.append(h)
-	    continue
+		err_conn_flag = False
+		for k in range(0,5):
+			##try connecting for 5 times
+			try:
+				print 'Conneting ' + h +' ...',
+				tn.open(h,23)
+				print 'Done.'
+				break
+			except:
+				tn.close()
+				print 'Error' + ('. Try Again.' if k < 4 else '. Skip.')
+				err_conn_flag = True
+		if err_conn_flag:
+			err_hosts.append(h)
+			continue
 		
-	## read devs stats ##
-	tn.write('cgminer-api -o devs\n')
-
-	## read pools stats ##
-	tn.write('cgminer-api -o pools\n')
-
-	tn.write('exit\n')
-
-	tmp = tn.read_all().split('cgminer-api')
-
-	dev_stats = tmp[-2]
-	pool_stats = tmp[-1]	
-
-	#print dev_stats
-	#print pool_stats
-
-	tn.close()
-
-	stats[h] = len(dev_stats.split('|')[1:-1])
+		## read devs ##
+		tn.write('cgminer-api -o devs\n')
 	
-	j=1
-	for ds in dev_stats.split('|')[1:-1]:
-	    if dev_flag.search(ds):
-		err_devs.append([i,j])
-	    j = j + 1
+		## read stats ##
+		tn.write('cgminer-api -o stats\n')
 
-	j=1
-	for ps in pool_stats.split('|')[1:-1]:
-	    if not pool_flag.search(ps):
-		err_pools.append([i,j])
-	    j = j + 1
+		## read pools ##
+		tn.write('cgminer-api -o pools\n')
 
-	i = i + 1
-	
-	
-    return (stats,err_hosts,err_devs,err_pools)
+		tn.write('exit\n')
+
+		tmp = tn.read_all().split('cgminer-api')
+
+		##!!!!!!!!! Bug Warning:
+		##!!!!!!!!! Condition: Some dev gets down between running 'cgminer-api -o devs' & '... -o stats'
+		##!!!!!!!!! Result: Different dev num in dev_data & stat_data
+		dev_data = tmp[-3]
+		stat_data = tmp[-2]
+		pool_data = tmp[-1]
+
+		tn.close()
+		
+		miner = []
+		dev = []
+		pool = []
+
+		for dd in dev_data.split('|')[1:-1]:
+			dev_stat = []
+			dev_stat.append(elapsed_time_flag.search(dd).group(1))
+			dev_stat.append(total_Mh_flag.search(dd).group(1))
+			dev_stat.append(temperature_d_flag.search(dd).group(1))
+			dev.append(dev_stat)
+			
+		j = 0
+		for sd in stat_data.split('|')[1:-1]:
+			if stat_pool_flag.search(sd) != None:
+				#ignore pool stat in ' -o stats'
+				break
+			
+			dev[j].append(len(module_id_flag.findall(sd)))
+			
+			temp = []
+			for t in temperature_s_flag.findall(sd):
+				temp.append(t.split('=')[1])
+			dev[j].append(temp)
+			
+			fan = []
+			for f in fan_flag.findall(sd):
+				fan.append(f.split('=')[1])
+			dev[j].append(fan)
+			
+			j += 1
+		
+		for pd in pool_data.split('|')[1:-1]:
+			pool_stat = []
+			pool_stat.append(pool_url_flag.search(pd).group(1))
+			pool_stat.append(pool_alive_flag.search(pd).group(1))
+			pool.append(pool_stat)
+		
+		miner.append(h)
+		miner.append(dev)
+		miner.append(pool)
+		data.append(miner)
+		i += 1
+		
+	return data
 
 if __name__ == '__main__':
-
-    hosts=[]
-    f = open('minerlist.txt','r')
-    lines = f.read().split('\n')
-    for line in lines:
-        if line == '':
-            continue
-        hosts.append(line)
-
-    (err_devs,err_pools)=chkstat(hosts)
-    #print err_devs
-    #print err_pools
-
+	hosts=[]
+	f = open('minerlist.txt','r')
+	lines = f.read().split('\n')
+	for line in lines:
+		if line == '':
+			continue
+		hosts.append(line)
+	data = chkstat(hosts)
+	print data
+	for miner in data:
+		print miner[0] + ':'
+		i = 1
+		for dev_stat in miner[1]:
+			print '\tModule #' + str(i) + ':'
+			print '\t\tDevice Elapsed: ' + dev_stat[0]
+			print '\t\tTotal MH: ' + dev_stat[1]
+			print '\t\tTemperature: ' + dev_stat[2]
+			print '\t\tModules Number: ' + str(dev_stat[3])
+			print '\t\tTemperature List: ' + ','.join(dev_stat[4])
+			print '\t\tFan Speed List: ' + ','.join(dev_stat[5])
+			i += 1
+		
+		i = 1
+		for pool_stat in miner[2]:
+			print '\tPool #' + str(i) + ':'
+			print '\t\tDevice Elapsed: ' + pool_stat[0]
+			print '\t\tStatus: ' + pool_stat[1]
+			i += 1
+		print "------------------------------------------------------------------------------"
+		
--- statreport/statreport.py
+++ statreport/statreport.py
@@ -3,77 +3,55 @@ from send_mail import send_mail
 from chkstat import chkstat
 import datetime
 
+MOD_NUM_PER_MINER = 10
+
 if __name__ == '__main__':
-    f = open('email.conf', 'r')
-    lines = f.read().split('\n')
-    mail={}
-    for line in lines:
-	if line=='':
-	    continue
-        tmp = line.split('=')
-        mail[tmp[0]] = tmp[1]
+	f = open('email.conf', 'r')
+	lines = f.read().split('\n')
+	mail={}
+	for line in lines:
+		if line=='':
+			continue
+		tmp = line.split('=')
+		mail[tmp[0]] = tmp[1]
 
-    hosts=[]
-    f = open('minerlist.txt','r')
-    lines = f.read().split('\n')
-    for line in lines:
-	if line == '':
-	    continue
-        hosts.append(line)
+	hosts=[]
+	f = open('minerlist.txt','r')
+	lines = f.read().split('\n')
+	for line in lines:
+		if line == '':
+			continue
+		hosts.append(line)
 
-    mail['MAIL_USER']=mail['FROM_EMAIL_ADDRESS'].split('@')[0]
-    
+	mail['MAIL_USER'] = mail['FROM_EMAIL_ADDRESS'].split('@')[0]
+	mail['SUBJECT'] = "[Miner Status P] Report " + datetime.datetime.now().strftime("%Y.%m.%d %H:%M")
 
-    mail['SUBJECT']="[Miner Status P] Report " + datetime.datetime.now().strftime("%Y.%m.%d %H:%M")
+	data = chkstat(hosts)
 
-    (stats,err_hosts,err_devs,err_pools)=chkstat(hosts)
+	mail['CONTENT'] = '''<html>
+							<body>
+								<a href="https://cex.io/r/0/canaan/0/" title="CEX.IO - Trade Ghashes while they mine you Bitcoins!" target="_blank">	
+									<img src="http://cex.io/informer/canaan/286ea7ed8c69fe6c98fcefeccb273fab/" width="500" height="35" border="0">
+								</a>'''
+	mail['CONTENT'] += "<p>Active IP Num: " + str(len(data)) + '/' + str(len(hosts)) + "</p>"
 
-    #print stats
+	mail['CONTENT'] += '''<p>
+							<table border="1">
+								<tr>
+									<th>IP</th>
+									<th>Active Dev Num</th>
+								</tr>'''
+	for miner in data:
+		sum_mod_num = 0
+		for dev_stat in miner[1]:
+			sum_mod_num += dev_stat[3]
+		if sum_mod_num < MOD_NUM_PER_MINER:
+			mail['CONTENT'] += '<tr><th>' + miner[0] + "</th><th>" + str(sum_mod_num) + "</th></tr>"
+	mail['CONTENT'] += '</table></p>' 
+	mail['CONTENT'] += '</body></html>'
+	print mail['CONTENT']
 
-    print len(hosts)
-    print len(err_hosts)
-
-    mail['CONTENT'] = '''<html>
-                           <body>
-                             <a href="https://cex.io/r/0/canaan/0/" title="CEX.IO - Trade Ghashes while they mine you Bitcoins!" target="_blank">
-                              <img src="http://cex.io/informer/canaan/286ea7ed8c69fe6c98fcefeccb273fab/" width="500" height="35" border="0">
-                             </a>'''
-    mail['CONTENT'] += "<p>Active IP Num: " + str(len(hosts) - len(err_hosts)) + "</p>"
-
-    mail['CONTENT'] += '''<p>
-                            <table border="1">
-                              <tr>
-                                <th>IP</th>
-                                <th>Active Dev Num</th>
-                              </tr>'''
-    for key in stats:
-	if stats[key]<10:
-	    mail['CONTENT'] += '<tr><th>' + key + "</th><th>" + str(stats[key]) + "</th></tr>"
-    mail['CONTENT'] += '</table></p>' 
-    #if err_devs:
-#	mail['CONTENT'] += '<table border="1"><tr><th>IP</th><th>Dev #</th></tr>'
-#	for e in err_devs:
-#	    mail['CONTENT'] += "<tr><th>" + hosts[e[0]-1]+ "</th><th>" + str(e[1]) + "</th></tr>"
-#	mail['CONTENT'] += '</table></p>'
-#    else:
-#	mail['CONTENT'] += "None.</p>"
-
-
-#    mail['CONTENT'] += "<p>Pools:<br>"
-#    if err_pools:
-#        mail['CONTENT'] += '<table border="1"><tr><th>IP</th><th>Pool #</th></tr>'	
-#        for e in err_pools:
-#            mail['CONTENT'] += "<tr><th>" + hosts[e[0]-1]+ "</th><th>" + str(e[1]) + "</th></tr>"
-#	mail['CONTENT'] += '</table></p>'
-#    else:
-#        mail['CONTENT'] += "None.</p>"
-#
-#    mail['CONTENT'] += '</body></html>'
-	
-    mail['CONTENT'] += '</body></html>'
-    print mail['CONTENT']
-
-    if send_mail(mail):
-        print "Successed."
-    else:
-	print "Failed"
+	if send_mail(mail):
+		print "Successed."
+	else:
+		print "Failed"
